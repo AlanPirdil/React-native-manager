3ee2bf122e8dbf4e6c9864376cb214d7e1b9d009
Alan Pirdil <pirdil.alan@gmail.com>
2017-07-06 16:56:03 +0300

    Wiring up email input with redux

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            value={this.props.email}
             label="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
           />
         </CardSection>
         <CardSection>
@@ -28,4 +37,10 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
