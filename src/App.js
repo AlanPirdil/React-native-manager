2c5fe6044be8e25701e234196437bfc8f7cf2b3b
Alan Pirdil <pirdil.alan@gmail.com>
2017-07-06 15:42:15 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import config from '../firebase-config';
+
+class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
