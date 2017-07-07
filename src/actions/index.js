8e958b6d2a30cd0202b4636057e949553e30d113
Alan Pirdil <pirdil.alan@gmail.com>
2017-07-07 13:02:03 +0300

    Route Employees and Create Employee -screens

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -24,7 +26,7 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
-    
+
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
@@ -40,6 +42,8 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.main();
 };
 
 const loginUserFailed = (dispatch) => {
